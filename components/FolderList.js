7ac517ed4c45e50ceed3de1252e032450861bdb4
prasannals <prasanna.pessu@gmail.com>
2018-12-17 12:43:59 +0530

    Added default message when no folder is present

--- components/FolderList.js
+++ components/FolderList.js
@@ -8,23 +8,32 @@ import Folder from './Folder';
 
 class FolderList extends React.Component {
   render() {
-    return (<ScrollView style={styles.scrollContainer}>
-      <View style={styles.container}>
-        {this.props.folders.map(folder => {
-          console.log(folder);
-          return <View
-                  style={styles.folderContainer}
-                  key={folder}
-                >
-                  <Folder
-                    folderName={folder}
-                    navigation={this.props.navigation}
-                    onDeleteFolder={this.props.onDeleteFolder}
-                  />
-                </View>;
-        })}
-      </View>
-    </ScrollView>);
+    if(this.props.folders.length == 0) {
+      return (<ScrollView style={styles.scrollContainer}>
+        <View style={styles.containerTextCenter}>
+          <Text>Please add an image. No folders available to display.</Text>
+        </View>
+      </ScrollView>);
+    } else {
+      return (<ScrollView style={styles.scrollContainer}>
+        <View style={styles.container}>
+          {this.props.folders.map(folder => {
+            console.log(folder);
+            return <View
+                    style={styles.folderContainer}
+                    key={folder}
+                  >
+                    <Folder
+                      folderName={folder}
+                      navigation={this.props.navigation}
+                      onDeleteFolder={this.props.onDeleteFolder}
+                    />
+                  </View>;
+          })}
+        </View>
+      </ScrollView>);
+    }
+
   }
 }
 
@@ -34,6 +43,11 @@ const styles = {
   },
   container: {
     flex: 1
+  },
+  containerTextCenter: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   folder: {
 
