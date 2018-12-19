8de1f2159563ecb36537960c2683bdeeda0ebaec
prasannals <prasanna.pessu@gmail.com>
2018-12-19 12:19:52 +0530

    Added support for training parameters

--- screens/TrainScreen.js
+++ screens/TrainScreen.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Button from 'apsl-react-native-button';
 import {
   View,
-  AsyncStorage
+  AsyncStorage,
+  TextInput
 } from 'react-native';
 import Strings from '../constants/Strings';
 import ConstObj from '../constants/Objects';
@@ -12,9 +13,50 @@ class TrainScreen extends React.Component {
     title: 'Train'
   };
 
+  state = {
+    paramViews: [],
+    params: []
+  }
+
+  createParamView(idx, onChangeKey, onChangeValue){
+    return (<View key={idx} style={{flex:1, flexDirection:'row'}}>
+      <TextInput
+        placeholder={'Key'}
+        style={styles.textInputStyle}
+        onChangeText={onChangeKey}
+        editable={true}
+      />
+      <TextInput
+        placeholder={'Value'}
+        style={styles.textInputStyle}
+        onChangeText={onChangeValue}
+        editable={true}
+      />
+    </View>);
+  }
+
+  addParamPressed() {
+    idx = this.state.paramViews.length;
+    this.state.params = this.state.params.concat({key:'', value:''})
+    this.state.paramViews = this.state.paramViews.concat(this.createParamView(idx,
+                          key => this.state.params[idx].key = key,
+                          value => this.state.params[idx].value = value))
+    console.log(this.state);
+    this.setState(this.state);
+  }
+
   render() {
     console.log(ConstObj.buttonTextStyle);
     return (<View style={styles.container}>
+      <View style={{flex:1}}>
+        {this.state.paramViews}
+      </View>
+      <Button
+        textStyle={ConstObj.buttonTextStyle}
+        onPress={this.addParamPressed.bind(this)}
+        style={ConstObj.buttonStyle}>
+        Add Parameter
+      </Button>
       <Button
         textStyle={ConstObj.buttonTextStyle}
         onPress={this.trainPressed.bind(this)}
@@ -33,7 +75,9 @@ class TrainScreen extends React.Component {
         trainEndpoint = endpoint + '/train';
         // console.log(trainEndpoint);
         fetch(trainEndpoint, {
-          method: 'GET'
+          method: 'POST',
+          body: JSON.stringify(this.state.params.filter(e => e.key !== '' && e.value !== '')), 
+          headers: { 'Content-type': 'application/json' }
         });
         console.log('Train Request Sent');
       })
@@ -44,12 +88,15 @@ class TrainScreen extends React.Component {
 const styles = {
   container: {
     flex: 1,
-    alignItems: 'center',
     backgroundColor: '#fff',
     paddingTop: 8,
     paddingRight: 8,
     paddingLeft: 8,
     paddingBottom: 8,
+  },
+  textInputStyle: {
+    height: 40,
+    width: '45%'
   }
 }
 
